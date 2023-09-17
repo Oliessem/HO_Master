8cae147d6bf43640df2ccc327aa101b1cce53cfb
Oliessem <117500793+Oliessem@users.noreply.github.com>
2023-09-17 19:06:54 -0300

    Update index.js

--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -4,12 +4,17 @@ const Sobre = () => {
   return (
     <div className="container-sobre h-screen md:h-screen lg:h-screen sm:h-full">
       <p>
-       A SOHL Arquitetura e Engenharia possui mais de 10 anos de experiência em projetos e serviços para a construção civil.
-       Entre suas atividades estão:
-       - Obras comerciais, residenciais e multifamiliares na região de Joinville.
-       - Obras Industriais em Santa Catarina e Paraná.
+       A SOHL Arquitetura e Engenharia possui mais de 10 anos de experiência em projetos e serviços para a construção civil. <br/>
+       Entre suas atividades estão: <br/>
+       - Obras comerciais, residenciais e multifamiliares na região de Joinville. <br/>
+       - Obras Industriais em Santa Catarina e Paraná. <br/>
 
-         Atualmente tem se especializado em atendimento on-line.
+         Atualmente tem se especializado em atendimento on-line. <br/>
+
+    A Arquiteta  <br/>
+
+    A Arquiteta Suellen Bona L., conhecida por seus Clientes como Sula Bona, é Graduada em Arquitetura e Urbanismo pela Unisociesc, Pós-Graduada em Interiores e Biodiversidade, fez Intercâmbio em Madrid- Espanha, e hoje está a frente do Escritório da SOHL Arquitetura & Engenharia, com uma equipe capacitada para atender, desenvolver e realizar os Sonhos de todos os clientes que confiam em nosso trabalho, e juntos transformamos esses Sonhos em Realidade.
+
        
       <hr/>
      
